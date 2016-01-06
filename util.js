32acec743235d896d20cb59e9a3049de5bc9aa1a
Fredrik Blomqvist <fblomqvist@gmail.com>
2016-01-07 00:04:40 +0100

    added createBlob helper

--- util.js
+++ util.js
@@ -1,7 +1,45 @@
-
+/**
+ *
+ * @author Fredrik Blomqvist
+ *
+ */
 define(['blq/assert'], function(assert) {
 
 var util = {};
+
+
+/**
+ * Factory to create Blobs. fallback to old BlobBuilder API.
+ * Mostly for better Android support.
+ * (this is not a polyfill. Doesn't emulate the 'slice' method etc).
+ *
+ * @see http://caniuse.com/#feat=blobbuilder Android 46+ (4.1+ with fallback). IE10+(not with JS content?)
+ *
+ * @param {Array.<string|Array|Blob|ArrayBuffer|ArrayBufferView>} parts
+ * @param {string} mimeType (todo: reasonable with some default maybe?)
+ * @return {Blob} null if failed
+ */
+util._createBlob = function(parts, mimeType) {
+	parts = parts || [];
+
+	var blob = null;
+	try {
+		blob = new Blob(parts, { type: mimeType });
+	} catch(ex) {
+		try {
+			var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+			var bb = new BlobBuilder();
+			parts.forEach(function(part) {
+				bb.append(part);
+			});
+			blob = bb.getBlob(mimeType);
+		} catch(ex2) {
+			console.error('createBlob: Both "Blob" and "BlobBuilder" failed. Unsupported or invalid input.', ex2);
+		}
+	}
+	return blob;
+};
+
 
 /**
  * Replaces "<"" etc with the corresponding "&lt;" chars to be able to display in html.
