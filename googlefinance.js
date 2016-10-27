93b0fd6e0ef6e8e5a1cf83e603fa48117f534a65
Fredrik Blomqvist <fblomqvist@gmail.com>
2016-10-27 11:33:02 +0200

    minor code improve

--- googlefinance.js
+++ googlefinance.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview
  * Small helper for the - now deprecated - Google Finance API.
- * still seems to works though.
+ * still seem to works though.
  *
  * @author Fredrik Blomqvist
  */
@@ -12,31 +12,32 @@ var finance = {
 	url: 'https://www.google.com/finance/info'
 };
 
-// todo: the values could be "refined" to more fitting types. now always strings it seems..
-// @see https://www.quora.com/What-do-the-following-attributes-returned-in-a-JSON-array-from-the-Google-Finance-API-mean
-// @see http://www.quantatrisk.com/2015/05/07/hacking-google-finance-in-pre-market-trading-python/
-
-/* example return:
+/*
+ todo: the values could be "refined" to more fitting types. now always strings it seems.. -> store name + type propagator fn?
+ @see https://www.quora.com/What-do-the-following-attributes-returned-in-a-JSON-array-from-the-Google-Finance-API-mean
+ @see http://www.quantatrisk.com/2015/05/07/hacking-google-finance-in-pre-market-trading-python/
+ 
+// example return (Alibaba stock):
 {
-c: "-1.02"
-c_fix: "-1.02"
-ccol: "chr"
-cp: "-1.20"
-cp_fix: "-1.20"
-e: "NYSE"
-id: "23536317556137"
-l: "83.78"
-l_cur: "83.78"
-l_fix: "83.78"
-lt: "Dec 24, 3:27PM EST"
-lt_dts: "2015-12-24T15:27:06Z"
-ltt: "3:27PM EST"
-pcls_fix: "84.8"
-s: "0"
-t: "BABA"
+	c: "-1.02",
+	c_fix: "-1.02",
+	ccol: "chr",
+	cp: "-1.20",
+	cp_fix: "-1.20",
+	e: "NYSE",
+	id: "23536317556137",
+	l: "83.78",
+	l_cur: "83.78",
+	l_fix: "83.78",
+	lt: "Dec 24, 3:27PM EST",
+	lt_dts: "2015-12-24T15:27:06Z",
+	ltt: "3:27PM EST",
+	pcls_fix: "84.8",
+	s: "0",
+	t: "BABA"
 }
 */
-var keyToFullName = { // todo: or store this in the fn for less exposure
+finance._keyToFullName = {
 	'id'     : 'ID',
 	't'      : 'StockSymbol',
 	'e'      : 'Index', // 'Exchange' ?
@@ -68,14 +69,16 @@ finance.getStockQuote = function(symbol) {
 	.then(function(ret) {
 		// remove odd blocker comment prefix
 		var json = ret.replace('//', '');
-		var res = JSON.parse(json);
+		return JSON.parse(json);
+	})
+	.then(function(res) {
 		// transform to long names (in-place)
 		res.forEach(function(stock) {
 			// grab keys up-front since we'll modify them
 			// (assuming no dups with the xfrm dict.. safest would be a clone/copy of course..)
 			var keys = Object.keys(stock);
 			keys.forEach(function(k) {
-				var newKey = keyToFullName[k];
+				var newKey = finance._keyToFullName[k];
 				if (newKey != null) {
 					stock[newKey] = stock[k];
 					delete stock[k];
