05a95fdb3f76778231094bf2ed97059efd8277ea
Fredrik Blomqvist <fblomqvist@gmail.com>
2017-01-27 10:43:46 +0100

    Changed to better default True logic if not supported and log.
    + some notes about polyfills. This wrapper is close to deprecated now on
    modern browsers I'd say.

--- pagevisibility.js
+++ pagevisibility.js
@@ -2,14 +2,15 @@
  * @fileoverview
  * Very simple lib for pagevis detection
  * @see http://canop.org/blog/?p=379b <- dead link..
- * todo: perhaps change to use a less "clever" non-overloaded API here? ..
+ * todo: perhaps change to use a less "clever" non-overloaded API here? .. (isVisible, subscribe, unsubscribe ?)
  *
  * usage:
- * pageVisible(function(){ .. });   // register callback
+ * pageVisible(function(isVisible){ .. });   // register callback
  * var visible = pageVisible(); // get current state
  *
  * turns into a NOP if feature not supported
  * @see http://caniuse.com/#feat=pagevisibility
+ * todo: seems like we could drop basically all polyfills now? 
  *
  * @author Fredrik Blomqvist
  */
@@ -17,16 +18,16 @@
 define([], function() {
 
 // namespace
-var pagevis = {};
+var api = {};
 
 /**
- *
- * @param {Function=} callback called when browser window/tab becomes visible or hidden. If no argument returns current state (boolean)
- * @return {boolean|Function}
+ * @param {function(boolean)=} callback called when browser window/tab becomes visible or hidden. If no argument returns current state
+ * @return {boolean}
  */
-pagevis.pageVisible = (function() {
+api.pageVisible = (function() {
 	var stateKey, eventKey, keys = {
 		hidden: 'visibilitychange',
+		// todo: all modern browser should support official name by now?
 		webkitHidden: 'webkitvisibilitychange',
 		mozHidden: 'mozvisibilitychange',
 		msHidden: 'msvisibilitychange'
@@ -41,21 +42,27 @@ pagevis.pageVisible = (function() {
 
 	// @blq edit added missing support case
 	if (eventKey == null) {
-		return function() {
-			// NOP
-			return false;
+		console.warn('Page Visibility not supported -> always return True');
+		return function(callback) {
+			if (callback) {
+				callback(true); // or after a tiny setTimeout?
+			}
+			return true;
 		};
 	}
 
 	return function(callback) {
-		if (callback) document.addEventListener(eventKey, function() {
-			callback(!document[stateKey]);
-		});
+		if (callback) {			
+			// todo: or return the bound fn (wrapper) here (treat it as a token/handle) so we could provide an removeEventListener?
+			document.addEventListener(eventKey, function() {
+				callback(!document[stateKey]);
+			});			
+		}
 		return !document[stateKey];
 	};
 })();
 
 
-return pagevis;
+return api;
 
 });
