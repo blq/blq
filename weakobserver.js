08e6406ca66a6c07176c8211dbdfa2eb02ec993a
blq <fblomqvist@gmail.com>
2018-09-23 16:50:41 +0200

    disconnectAllTo supports 2nd arg function. Fixed bug in Symbol usage

--- weakobserver.js
+++ weakobserver.js
@@ -11,7 +11,7 @@
  *
  * todo: support __connect__/__disconnect__ interceptors? (or use Symbols?)
  * todo: signal namespace support?
- * note: doesn't support the DOM-overload like MochiKit (think such should be other module)
+ * note: doesn't support the DOM-overload like MochiKit (think such stuff should be in other module)
  *
  */
 
@@ -34,25 +34,32 @@ var destStore = new WeakMap();
 // WeakMap->Set
 // obj->handles
 
-var __connect__ = Symbol['__connect__'];
+api.__connect__ = Symbol('__connect__');
 // todo: hmm, disconnect requires more code changes. maybe skip (MK didn't add it until later also I think(?))
-// var __disconnect__ = Symbol['__disconnect__'];
+// -> most importantly it makes fast disconnectAll a O(n) instead of O(1)...
+api.__disconnect__ = Symbol('__disconnect__');
 
 
 /**
  * http://blq.github.io/mochikit/doc/html/MochiKit/Signal.html#fn-connect
  * @param {Object} obj
  * @param {string} sig
- * @param {function} fn
+ * @param {Object|function} destOrFn
+ * @param {function=} fn
  * @return {Object} handle
  */
 api.connect = function(obj, sig, destOrFn, fn) {
+	// when using new Map we can actually skip this requirement(?) useful?
+    // if (typeof sig != 'string') {
+    //     throw new TypeError("'sig' must be a string");
+    // }
+
 	var dest;
-	if (arguments.length == 3) {
+	if (arguments.length === 3) {
 		fn = destOrFn;
 		// no need to bind fn since obj will be default call context
 	} else
-	if (arguments.length == 4) {
+	if (arguments.length === 4) {
 		dest = destOrFn;
 		fn = fn.bind(dest);
 	}
@@ -72,11 +79,12 @@ api.connect = function(obj, sig, destOrFn, fn) {
 
 	// should be treated as a black-box by outside code
 	// == MochiKit.Signal.Ident
+	// guess we could hide more here? even return hash-keys or such? (but no real win I think)
 	var handle = {
 		obj: obj,
 		sig: sig,
-		fn: fn,
-		dest: dest
+		dest: dest,
+		fn: fn
 	};
 
 	if (dest) {
@@ -88,10 +96,11 @@ api.connect = function(obj, sig, destOrFn, fn) {
 		handles.add(handle);
 	}
 
-	if (obj[__connect__]) {
+	var __connect = obj[api.__connect__] || obj['__connect__']; // support old non Symbol also
+	if (typeof __connect === 'function') {
 		var args = Array.from(arguments);
 		args[0] = handle;
-		obj[__connect__].apply(obj, args);
+		__connect.apply(obj, args);
 	}
 
 	return handle;
@@ -101,8 +110,10 @@ api.connect = function(obj, sig, destOrFn, fn) {
 // to call disconnect at teardowns. This is now only needed if
 // you need to disconnect during use, before a teardown. (and symmetry)
 
-/***
+/**
  * http://blq.github.io/mochikit/doc/html/MochiKit/Signal.html#fn-disconnectall
+ * @param {Object} handle
+ * @param {...string} signals
  */
 api.disconnectAll = function(handle, ...signals) {
 	if (signals.length > 0) {
@@ -110,7 +121,7 @@ api.disconnectAll = function(handle, ...signals) {
 		if (!m) return;
 		signals.forEach(m.delete, m);
 
-		if (m.size == 0) {
+		if (m.size === 0) {
 			store.delete(handle.obj);
 		}
 	} else {
@@ -120,44 +131,66 @@ api.disconnectAll = function(handle, ...signals) {
 
 /**
  * http://blq.github.io/mochikit/doc/html/MochiKit/Signal.html#fn-disconnect
+ * @param {Object} handle
+ *
  */
 api.disconnect = function(handle) {
 	var m = store.get(handle.obj); // Map
-	if (!m) return;
+	if (!m) return false;
 
 	var callbacks = m.get(handle.sig); // Set
-	if (!callbacks) return;
+	if (!callbacks) return false;
 
 	callbacks.delete(handle.fn);
-	if (callbacks.size == 0) {
+	if (callbacks.size === 0) {
 		m.delete(handle.sig);
 	}
 
 	if (handle.dest) {
 		var handles = destStore.get(handle.dest);
-		if (!handles) return; // if handle.dest exists this should always exist..
+		// if (!handles) return; // if handle.dest exists this should always exist..
 		handles.delete(handle);
 
-		if (handles.size == 0) {
+		if (handles.size === 0) { // todo: hmm, if deemed heavy we could delegate these kind of empty-DS deletes to a idle GC-task!?
 			destStore.delete(handle.dest);
 		}
 	}
 
-	if (m.size == 0) {
+	if (m.size === 0) {
 		store.delete(handle.obj);
 	}
 
-	// todo: worth it to support __disconnect__ intercept? (need to rewrite other disconnectAll etc)
+	// todo: worth it to support __disconnect__ intercept? (need to rewrite other disconnectAll etc! slower..)
+	var __disconnect = handle.obj[api.__disconnect__] || handle.obj['__disconnect__'];
+	if (typeof __disconnect === 'function') {
+		__disconnect(handle, handle.sig, handle.dest, handle.fn);
+	}
+	return true;
 };
 
 /**
  * http://blq.github.io/mochikit/doc/html/MochiKit/Signal.html#fn-disconnectallto
+ * todo: support 2nd arg 'fn' also? (would need to store original function in handler..)
+ * @param {Object} dest
+ * @param {function=} fn
  */
-api.disconnectAllTo = function(dest) {
+api.disconnectAllTo = function(dest, fn) {
 	var handles = destStore.get(dest);
 	if (!handles) return;
-	handles.forEach(api.disconnect);
-	destStore.delete(dest); // disconnect does this too now
+	if (typeof fn === 'function') {
+		handles.forEach(function(handle) {
+			// todo: bug! if fn is bound to dest these won't match! -> store original fn..
+			if (handle.fn === fn) {
+				api.disconnect(handle);
+			}
+		});
+		if (handles.size === 0) {
+			destStore.delete(dest); // disconnect does this too now
+		}
+	} else {
+		handles.forEach(api.disconnect);
+		destStore.delete(dest); // disconnect does this too now
+	}
 };
 
 
@@ -186,7 +219,7 @@ api.signal = function(obj, sig, ...args) {
 		}
 	});
 
-	if (errors.length == 1) {
+	if (errors.length === 1) {
 		throw errors[0];
 	} else
 	if (errors.length > 1) {
